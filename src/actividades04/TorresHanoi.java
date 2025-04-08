c3222d1797d5c538cda09f87eaf2f375733cfe0c
PieroMeza10 <pieroms0905@gmail.com>
2025-04-08 16:17:40 -0500

    torre3

--- src/actividades04/TorresHanoi.java
+++ src/actividades04/TorresHanoi.java
@@ -5,5 +5,10 @@ public class TorresHanoi {
         TorresHanoi(3, 1, 2, 3, )
 
     }
+    public static void TorresHanoi(int discos, int torre1, int torre2, int torre3){
+        if (discos==1) {
+            System.out.println("Mover disco de la torre" +torre1 + "hacia la torre" + torre2);
+        }
+    }
 
 }
