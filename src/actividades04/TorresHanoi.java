961f9da9bd256ae72faca931165b4af80e577637
PieroMeza10 <pieroms0905@gmail.com>
2025-04-08 16:11:10 -0500

    torre1

---
+++ src/actividades04/TorresHanoi.java
@@ -0,0 +1,9 @@
+package actividades04;
+
+public class TorresHanoi {
+    public static void main(String[] args) {
+        TorresHanoi(discos:3, torre1:1, torre2:2, torre3:3, )
+        
+    }
+
+}
