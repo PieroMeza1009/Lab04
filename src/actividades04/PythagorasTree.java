142a3a4d39a7119eef3be3e0dc37d5720aed271a
PieroMeza10 <pieroms0905@gmail.com>
2025-04-08 16:27:46 -0500

    pitagoras2

--- src/actividades04/PythagorasTree.java
+++ src/actividades04/PythagorasTree.java
@@ -5,6 +5,11 @@ import javax.swing.JPanel;
 public class PythagorasTree extends JPanel {
     private int profundidad;
 
-    
+    public PythagorasTree(int profundidad) {
+        this.profundidad = profundidad;
+        setPreferredSize(new Dimension(800, 800));
+    }    
+
+
 
 }
