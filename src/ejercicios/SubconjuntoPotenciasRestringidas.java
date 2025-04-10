9153c2129471c1470f6d3b0eca68b60d3ca45752
PieroMeza10 <pieroms0905@gmail.com>
2025-04-09 22:33:38 -0500

    ejer1.13

--- src/ejercicios/SubconjuntoPotenciasRestringidas.java
+++ src/ejercicios/SubconjuntoPotenciasRestringidas.java
@@ -68,4 +68,14 @@ public class SubconjuntoPotenciasRestringidas {
     return puedeAlcanzarObjetivo(nums, 0, nuevoObjetivo, obligatorios, excluidos, memo);
 
     }
+
+    public static void main(String[] args) {
+        int[][] entradas = {
+            {5, 2, 4, 8, 10, 3, 14},
+            {5, 4, 8, 10, 3, 5, 27},
+            {5, 4, 8, 10, 3, 6, 27},
+            {6, 2, 16, 5, 7, 10, 33},
+            {6, 2, 16, 5, 3, 10, 33},
+            {4, 2, 5, 1, 6, 13}
+        };
 }
