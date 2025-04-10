94fe7f10fcc5cee775107199e4dc185357ad138d
PieroMeza10 <pieroms0905@gmail.com>
2025-04-09 22:09:07 -0500

    ejer1.3

--- src/ejercicios/SubconjuntoPotenciasRestringidas.java
+++ src/ejercicios/SubconjuntoPotenciasRestringidas.java
@@ -3,9 +3,16 @@ package ejercicios;
 import java.util.*;
 
 public class SubconjuntoPotenciasRestringidas {
-    
+
     public static boolean esPotenciaDeDos(int n) {
         return n > 0 && (n & (n - 1)) == 0;
+    }
+
+    public static boolean puedeAlcanzarObjetivo(int[] nums, int index, int objetivo, Set<Integer> obligatorios, Set<Integer> excluidos, Map<String, Boolean> memo) {
+        if (objetivo == 0) return true;
+        if (index == nums.length) return false;
+    
+    
     }
 
 }
