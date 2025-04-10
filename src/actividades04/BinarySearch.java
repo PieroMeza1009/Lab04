8a8297fc252a107c23c70d3d664458c77cc4d335
PieroMeza10 <pieroms0905@gmail.com>
2025-04-09 21:32:53 -0500

    b4

--- src/actividades04/BinarySearch.java
+++ src/actividades04/BinarySearch.java
@@ -5,7 +5,12 @@ public class BinarySearch {
     int binarySearch(int arr[], int lo, int hi, int x){
 
         if(hi >= lo && lo < arr.length - 1) {
-            
+            int mid = lo + (hi - lo) / 2;
+            if (arr[mid] == x)
+            return mid;
+
+
+
         }
 
     }
