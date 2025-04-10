67d618b7cbffae46a69597deafac09948622acdd
PieroMeza10 <pieroms0905@gmail.com>
2025-04-09 22:07:46 -0500

    ejer1.2

--- src/ejercicios/SubconjuntoPotenciasRestringidas.java
+++ src/ejercicios/SubconjuntoPotenciasRestringidas.java
@@ -3,5 +3,9 @@ package ejercicios;
 import java.util.*;
 
 public class SubconjuntoPotenciasRestringidas {
+    
+    public static boolean esPotenciaDeDos(int n) {
+        return n > 0 && (n & (n - 1)) == 0;
+    }
 
 }
